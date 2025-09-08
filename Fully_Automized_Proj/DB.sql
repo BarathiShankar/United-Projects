7dbf045afcab347f9d18a8ac82586511b0166659
Barathi Shankar <barathishankarr@gmail.com>
2025-09-08 22:47:01 +0530

    Update DB.sql
    Created Table for Login data

--- Fully_Automized_Proj/DB.sql
+++ Fully_Automized_Proj/DB.sql
@@ -4,3 +4,5 @@ CREATE TABLE STUDENT(Name VARCHAR(50), RN INT PRIMARY KEY,Address VARCHAR(200),G
   longitude DECIMAL(11, 8),Status ENUM('PRESENT', 'ABSENT', 'LATE'),totpre INT DEFAULT 0,totabs INT DEFAULT 0);
 --Table to store Staff data and class information 
 CREATE TABLE STAFF(Name VARCHAR(50),SID INT PRIMARY KEY,Email VARCHAR(50),latitude  DECIMAL(10, 8),longitude DECIMAL(11, 8));
+--Table to store the data used for login purpose
+CREATE TABLE LOGIN ( UserID INT PRIMARY KEY,Role ENUM('STUDENT', 'STAFF', 'ADMIN'),Username VARCHAR(50) UNIQUE,PasswordHash VARCHAR(255),LastLogin DATETIME);
